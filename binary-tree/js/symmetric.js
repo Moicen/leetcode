8b2e0dfc0ee62c9ea5fed2a03bad4d990992ded9
moicen <i@moicen.com>
2019-11-22 13:47:28 +0800

    判断是否对称树，循环方式

--- binary-tree/js/symmetric.js
+++ binary-tree/js/symmetric.js
@@ -26,6 +26,10 @@ const tree2 = {
     val: 1
 };
 
+const tree3 = {
+
+}
+
 const recur = (origin, target) => {
     if (origin && target) {
         let same = origin.val === target.val;
@@ -34,17 +38,46 @@ const recur = (origin, target) => {
     return !origin && !target;
 };
 
-const isSymmetric = function (root) {
+const iterate = (root) => {
+    if (!root) return true;
+
+    const pairs = [{left: root.left, right: root.right}];
+    while (pairs.length > 0) {
+        let {left, right} = pairs.pop();
+        if (!left && !right) {
+            continue;
+        }
+        if (left && right) {
+            if (left.val === right.val) {
+                pairs.push({left: left.left, right: right.right});
+                pairs.push({left: right.left, right: left.right})
+                continue;
+            }
+            return false;
+        }
+        return false;
+    }
+    return true;
+};
+
+const recurWalk = (root) => {
     log("Tree: ");
     log(root, true);
     if (!root) return true;
     return recur(root.left, root.right);
 };
 
+const iterateWalk = (root) => {
+    log("Tree: ");
+    log(root, true);
+    return iterate(root);
+}
 
 run([
-    {desc: 'Test symmetric tree1 (use recur)', exec: () => isSymmetric(tree1)},
-    {desc: 'Test symmetric tree2 (use recur)', exec: () => isSymmetric(tree2)},
+    {desc: 'Test symmetric tree1 (use recur)', exec: () => recurWalk(tree1)},
+    {desc: 'Test symmetric tree2 (use recur)', exec: () => recurWalk(tree2)},
+    {desc: 'Test symmetric tree1 (use iterate)', exec: () => iterateWalk(tree1)},
+    {desc: 'Test symmetric tree2 (use iterate)', exec: () => iterateWalk(tree2)},
 ]);
 
 
