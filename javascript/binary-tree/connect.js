271888dc44b43dbe681f82b6a0e6012bc78e8422
moicen <i@moicen.com>
2019-11-25 10:38:56 +0800

    connect

--- javascript/binary-tree/connect.js
+++ javascript/binary-tree/connect.js
@@ -1,148 +1,142 @@
 const run = require('./runner');
 
-const PerfectTree1 = {
+const Tree1 = {
     "$id": "1",
     "left": {
         "$id": "2",
-        "left": {
-            "$id": "3",
-            "left": null,
-            "next": null,
-            "right": null,
-            "val": 4
-        },
+        "left": null,
         "next": null,
-        "right": {
+        "right": null,
+        "val": 9
+    },
+    "next": null,
+    "right": {
+        "$id": "3",
+        "left": {
             "$id": "4",
             "left": null,
             "next": null,
             "right": null,
-            "val": 5
-        },
-        "val": 2
-    },
-    "next": null,
-    "right": {
-        "$id": "5",
-        "left": {
-            "$id": "6",
-            "left": null,
-            "next": null,
-            "right": null,
-            "val": 6
+            "val": 15
         },
         "next": null,
         "right": {
-            "$id": "7",
+            "$id": "5",
             "left": null,
             "next": null,
             "right": null,
             "val": 7
         },
-        "val": 3
+        "val": 20
     },
-    "val": 1
+    "val": 3
 };
-const PerfectTree2 = {
+const Tree2 = {
     "$id": "1",
     "left": {
         "$id": "2",
         "left": {
             "$id": "3",
-            "left": null,
+            "left": {
+                "$id": "4",
+                "left": null,
+                "next": null,
+                "right": null,
+                "val": 5
+            },
             "next": null,
-            "right": null,
-            "val": 4
+            "right": {
+                "$id": "5",
+                "left": null,
+                "next": null,
+                "right": null,
+                "val": 1
+            },
+            "val": 1
         },
         "next": null,
-        "right": {
-            "$id": "4",
-            "left": null,
-            "next": null,
-            "right": null,
-            "val": 5
-        },
+        "right": null,
         "val": 2
     },
     "next": null,
     "right": {
-        "$id": "5",
+        "$id": "6",
         "left": {
-            "$id": "6",
-            "left": null,
-            "next": null,
-            "right": null,
-            "val": 6
-        },
-        "next": null,
-        "right": {
             "$id": "7",
             "left": null,
             "next": null,
-            "right": null,
-            "val": 7
+            "right": {
+                "$id": "8",
+                "left": null,
+                "next": null,
+                "right": null,
+                "val": 6
+            },
+            "val": 3
         },
-        "val": 3
+        "next": null,
+        "right": {
+            "$id": "9",
+            "left": null,
+            "next": null,
+            "right": {
+                "$id": "10",
+                "left": null,
+                "next": null,
+                "right": null,
+                "val": 8
+            },
+            "val": -1
+        },
+        "val": 4
     },
-    "val": 1
+    "val": 0
 };
 
 /**
  * 将二叉树的每个节点的next属性指向同层右边的节点
- * @param root
+ * @param nodes
  */
 
-const recur = (root) => {
-    if (!root) return;
-
-    let {left, right} = root;
-    if (left) left.next = right || null;
-    if (right) {
-        let uncle = root.next;
-        if (uncle) right.next = uncle.left || null;
-    }
-    recur(left);
-    recur(right);
+const recur = (nodes) => {
+    if (nodes.length === 0) return;
+    let children = [];
+    nodes.forEach((node, index) => {
+        node.next = nodes[index + 1] || null;
+        let {left, right} = node;
+        if (left) children.push(left);
+        if (right) children.push(right);
+    });
+    recur(children)
 };
 
 const recurWalk = (tree) => {
-    console.log("Perfect tree: ", tree);
-    recur(tree);
+    recur([tree]);
     return tree;
 };
 
 const iterate = (root) => {
     if (!root) return;
-    const stack = [root], routes = [];
-    let node;
-    while ((node = stack.pop())) {
-        routes.push(node);
-        let {left, right} = node;
-        if (left && !routes.includes(left)) {
-            left.next = right;
-            stack.push(node);
-            stack.push(left);
-            continue;
-        }
-        if (right && !routes.includes(right)) {
-            let uncle = node.next;
-            if (uncle && right.next !== uncle.left) {
-                right.next = uncle.left;
-            }
-            stack.push(node);
-            stack.push(right);
-            continue;
-        }
+    let parent = [], current = [];
+    parent.push(root);
+    while (parent.length) {
+        parent.forEach((node, index) => {
+            node.next = parent[index + 1] || null;
+            let {left, right} = node;
+            if (left) current.push(left);
+            if (right) current.push(right);
+        })
+        parent = current;
+        current = [];
     }
 };
 
 const iterateWalk = (tree) => {
-    console.log("Perfect tree: ", tree);
     iterate(tree);
     return tree;
 };
 
 run([
-    {desc: "connect perfect tree with recur: ", exec: () => recurWalk(PerfectTree1), json: true},
-    {desc: "connect perfect tree with iterate: ", exec: () => iterateWalk(PerfectTree2), json: true}
+    {desc: "connect tree with recur: ", exec: () => recurWalk(Tree1), json: true},
+    {desc: "connect tree with iterate: ", exec: () => iterateWalk(Tree2), json: true},
 ], false);
